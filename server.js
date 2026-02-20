d96a910c5a37805aee201d41c7feb1cb3a152ac0
Ahsaan396 <ahsanulhakim285@gmail.com>
2026-02-21 04:32:50 +0600

    Product CRUD added. An admin can add,update,delete product now.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// MongoDB Connection (Replace with your URI or use local)
+
 mongoose.connect(process.env.mongo_uri)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
@@ -20,3 +20,10 @@ app.use('/api/auth', authRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
+
+// Connect your routes
+app.use('/api/products', require('./routes/product'));
